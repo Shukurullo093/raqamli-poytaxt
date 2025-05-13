328aaa6e159ec1c5bec583e28066e6bbb5d5ddb8
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2025-05-13 16:49:28 +0500

    admin create, delete, filter

--- public/js/login.js
+++ public/js/login.js
@@ -3,8 +3,7 @@ let baseUrl = 'http://localhost:3000'
 document.getElementById('loginForm').addEventListener('submit', async function(e) {
     e.preventDefault();
     const login = document.getElementById('username').value;
-    const telefon = document.getElementById('password').value;
-    // console.log(dataForm);
+    const telefon = document.getElementById('password').value.replace(' ', '');
 
     const res = await fetch(baseUrl + '/admin/login', {
         method: 'POST',
@@ -13,12 +12,18 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     });
 
     const data = await res.json();
-    if(res.ok){
-        console.log(data.token);
-        console.log(data.token.token)
-        localStorage.setItem('Authorization', 'Bearer' + data.token.token);
-        window.location.href = 'index.html'
-    } else{
-        console.log(data.error);
+    if(res.ok) {
+        // console.log(data.token.admin.id);
+        // console.log(data.token.token)
+        localStorage.setItem('adminId', data.token.admin.id);
+        localStorage.setItem('token', 'Bearer ' + data.token.token);
+        window.location.href = data.token.admin.vazifa == 'Admin' ? 'index.html' : 'm24-admin/index.html'
+    } else {
+        const error = document.getElementById('error');
+        error.style.display = 'block';
+        error.innerHTML = `<i class="bi bi-exclamation-circle text-danger"></i> ${data.error}`;
+        setTimeout(()=>{
+            error.style.display = 'none';
+        }, 5000);
     }    
 })
--- public/m24-admin/js/script.js
+++ public/m24-admin/js/script.js
@@ -1,3 +1,25 @@
+let baseUrl = 'http://localhost:3000'
+
+window.onload = async function(){
+    const id = localStorage.getItem('adminId');
+    if (id !== null){
+        const res = await fetch(baseUrl + `/admin/${id}`, {
+            method: 'GET',
+            // headers: { 'Content-Type': 'application/json' },
+            // body: JSON.stringify({ login, telefon })
+        });
+
+        const data = await res.json();
+        console.log(data);
+        document.getElementById('adminName').innerText = formattedName(data.FISH);
+        document.getElementById('role').innerText = data.vazifa;
+    }
+}
+
+function formattedName(fullname){
+    return fullname.split(' ')[0] + ' ' + fullname.split(' ')[1][0] + '.'
+}
+
 ymaps.ready(init);
 
 function init() {
