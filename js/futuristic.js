10103916fa25bddaa5d63e5b1a4444988b5634a5
Shukurullo093 <nazarqosimovshukurullo093@gmail.com>
2025-06-10 17:11:09 +0500

    tashkent border

--- js/futuristic.js
+++ js/futuristic.js
@@ -8,50 +8,6 @@ function closeModal() {
     document.getElementById('modal').classList.remove('active');
 }
 
-// Make modal draggable
-// let isDragging = false;
-// let currentX;
-// let currentY;
-// let initialX;
-// let initialY;
-// let xOffset = 0;
-// let yOffset = 0;
-
-// const modal = document.getElementById('modal');
-// const modalHeader = document.getElementById('modalHeader');
-
-// modalHeader.addEventListener('mousedown', dragStart);
-// document.addEventListener('mousemove', drag);
-// document.addEventListener('mouseup', dragEnd);
-
-// function dragStart(e) {
-//     initialX = e.clientX - xOffset;
-//     initialY = e.clientY - yOffset;
-
-//     if (e.target === modalHeader || modalHeader.contains(e.target)) {
-//         isDragging = true;
-//     }
-// }
-
-// function drag(e) {
-//     if (isDragging) {
-//         e.preventDefault();
-//         currentX = e.clientX - initialX;
-//         currentY = e.clientY - initialY;
-//         xOffset = currentX;
-//         yOffset = currentY;
-
-//         modal.style.transform = `translateX(calc(-50% + ${currentX}px)) translateY(${currentY}px)`;
-//     }
-// }
-
-// function dragEnd(e) {
-//     initialX = currentX;
-//     initialY = currentY;
-//     isDragging = false;
-// }
-
-// Close modal with Escape key
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape') {
         closeModal();
